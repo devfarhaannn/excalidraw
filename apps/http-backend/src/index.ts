ae4bb353625cac52613be43dc8d8ecf5eb6d73b0
devfarhaannn <bhattfarhan1804@gmail.com>
2026-05-08 01:52:55 +0530

    Initial commit

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,53 @@
+
+import express from "express"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "@repo/backend-common/config"
+import { middleware } from "./middleware"
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
+
+const app = express()
+
+app.post("/signup", (req, res) => {
+    //db
+
+    const data = CreateUserSchema.safeParse(req.body)
+    if (!data.success) {
+        res.json({
+            message: "Incorrects input"
+        })
+        return;
+    }
+    res.json({
+        userid: 123
+    })
+})
+app.post("/signin", (req, res) => {
+    const data = SigninSchema.safeParse(req.body)
+    if (!data.success) {
+        res.json({
+            message: "Incorrects input"
+        })
+        return;
+    }
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+})
+app.post("/room", middleware, (req, res) => {
+
+    const data = CreateRoomSchema.safeParse(req.body)
+    if(!data.success){
+         res.json({
+            message : "Incorrects input"
+        })
+        return;
+    }
+    res.json({
+        roomId: 123
+    })
+
+})
+
+app.listen(3001)
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "@repo/backend-common/config";
+
+
+export function middleware(req:Request, res:Response, next:NextFunction){
+    const token  = req.headers["authorization"] ?? ""
+
+    const decoded = jwt.verify(token, JWT_SECRET)
+    if(decoded && typeof decoded !== "string"){
+        req.userId = decoded.userId
+        next()
+    }else{
+        res.status(404).json({
+            message:"Unauthorized"
+        })
+    }
+}
